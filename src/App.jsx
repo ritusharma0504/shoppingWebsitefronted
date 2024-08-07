cf096304f523f146bc0e24e6749f3d45d7d574f5
ritusharma0504 <ritusharma7924@gmail.com>
2024-08-07 16:48:28 +0530

    main code  react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import Carsouel from "./Components/Carsouel";
+import Card from "./Components/Card";
+import Fotter from "./Components/Fotter";
 function App() {
-
+  let [data, setData] = useState([]);
+  useEffect(() => {
+    const fun = async () => {
+      const response = await fetch("http://11circle.live/");
+      const jsonResponse = await response.json();
+      setData(jsonResponse["data"]["catalogs"]);
+    };
+    fun();
+  }, []);
+  // console.log(data);
   return (
     <>
-     <h1>ritu pancholi</h1>
+      <Navbar />
+      <Carsouel />
+      {data.map((item,index) => (
+        <Card one={item}/>
+      ))}
+      <Fotter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { useState } from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Navbar() {
+
+  return (
+    <>
+     <h1>Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
